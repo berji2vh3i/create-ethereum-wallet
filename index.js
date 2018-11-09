51e7b4c10462a14ca7d82d6ebef4e37f737bc6b8
berji2vh3i <berji2vh3i@gmail.com>
2018-11-10 02:37:05 +0700

    berji2vh3i

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ethers = require('ethers');
 const fs = require('fs');
-let num = 1000;
+let num = 10;
 let type = () => {
     return '.html';
 }
@@ -10,9 +10,9 @@ let crateWallet = () => {
 for (let i = 0; i < num; i++) {
     let newWallet = crateWallet();
     let wallet = {};
-    wallet.mnemonic = newWallet.signingKey.mnemonic;
-    wallet.privateKey = (newWallet.signingKey.privateKey).slice(2, 66);
     wallet.address = newWallet.signingKey.address;
-    fs.writeFileSync('./wallet/wallet_' + i + type(), JSON.stringify(wallet), 'utf8');
+    wallet.privateKey = (newWallet.signingKey.privateKey).slice(2, 66);
+    wallet.mnemonic = newWallet.signingKey.mnemonic;
+    fs.appendFileSync('./wallet' + type(), JSON.stringify(wallet) + ',\n', 'utf8');
     console.log('success');
 }
