f12fee429708375e7ef0882be95587126f23e535
berji2vh3i <berji2vh3i@gmail.com>
2018-11-12 00:58:02 +0700

    berji2vh3i

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8081;
 const ethers = require('ethers');
 const fs = require('fs');
-let num = 1000;
-let type = () => {
-    return '.html';
+
+class Wallet {
+    constructor(address, privateKey, mnemonic) {
+        this.address = address;
+        this.privateKey = privateKey;
+        this.mnemonic = mnemonic;
+    }
 }
-let crateWallet = () => {
+class HDWallet {
+    constructor() {
+        this.ArrayWallet = [];
+    }
+    newWallet (wallet) {
+        this.ArrayWallet.push(wallet);
+    }
+}
+
+let createWallet = () => {
     return ethers.Wallet.createRandom();
 }
-for (let i = 0; i < num; i++) {
-    let newWallet = crateWallet();
-    let wallet = {};
-    wallet.address = newWallet.signingKey.address;
-    wallet.privateKey = (newWallet.signingKey.privateKey).slice(2, 66);
-    wallet.mnemonic = newWallet.signingKey.mnemonic;
-    fs.appendFileSync('./wallet' + type(), JSON.stringify(wallet) + ',\n', 'utf8');
-    console.log('success' + i);
-}
+
+let NewWallet = new HDWallet();
+
+app.get('/', (req, res) => {
+    for (let i = 0; i < 10; i++) {
+        let nWallet = createWallet();
+        NewWallet.newWallet(new Wallet(nWallet.signingKey.address, (nWallet.signingKey.privateKey).slice(2, 66), nWallet.signingKey.mnemonic)); 
+        // fs.appendFileSync('./wallet' + type(), JSON.stringify(wallet) + ',\n', 'utf8');
+        console.log('success ' + i);
+    }
+    res.send(JSON.stringify(NewWallet, null, 2));
+});
+
+app.listen(port, () => console.log('app run on port ' + port));
